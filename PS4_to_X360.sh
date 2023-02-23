26aa42d38f81cd5f006efa33dd30d8ca5f64a3dc
Brenden Callahan <brendenkcallahan@gmail.com>
2023-02-23 12:32:35 -0800

    wireless option not working switching to wired instead

--- PS4_to_X360.sh
+++ PS4_to_X360.sh
@@ -12,7 +12,7 @@ while [ $conNum -gt 0 ]
 do
   echo -e "Enter the /eventid:"
   read id 
-  xboxdrv --mimic-xpad-wireless --silent --quiet --evdev /dev/input/event$id --evdev-absmap ABS_X=x1,ABS_Y=y1,ABS_RX=x2,ABS_RY=y2,ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y --axismap -Y1=Y1,-Y2=Y2 --evdev-keymap BTN_NORTH=y,BTN_SOUTH=a,BTN_EAST=b,BTN_WEST=x,BTN_SELECT=back,BTN_START=start,BTN_TR=rb,BTN_TR2=rt,BTN_TL=lb,BTN_TL2=lt,BTN_THUMBR=tr,BTN_THUMBL=tl &
+  xboxdrv --mimic-xpad --silent --quiet --evdev /dev/input/event$id --evdev-absmap ABS_X=x1,ABS_Y=y1,ABS_RX=x2,ABS_RY=y2,ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y --axismap -Y1=Y1,-Y2=Y2 --evdev-keymap BTN_NORTH=y,BTN_SOUTH=a,BTN_EAST=b,BTN_WEST=x,BTN_SELECT=back,BTN_START=start,BTN_TR=rb,BTN_TR2=rt,BTN_TL=lb,BTN_TL2=lt,BTN_THUMBR=tr,BTN_THUMBL=tl &
   echo -e "Listening to controller $conNum with id $id..."
   ((conNum--))
 done
